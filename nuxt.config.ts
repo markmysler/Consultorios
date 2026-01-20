a2ad6488b0233f950c4bef3c69ef53ee548008c5
Mark Mysler <104447445+markmysler@users.noreply.github.com>
2026-01-19 21:48:39 -0300

    Update CSS path in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     }
   },
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['@/assets/css/main.css'],
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
